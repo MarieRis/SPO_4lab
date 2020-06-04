5358b405651e0bbf958b609fe2c40817d106e94b
MarieRis <masshha7323@gmail.com>
2020-06-04 10:17:41 +0400

    Add files via upload

---
+++ SPO4lab/Class1.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Security.AccessControl;
+using System.IO;
+
+namespace SPO4lab
+
+{
+
+    
+       public class Program1
+        {
+        static public bool SetFullControl(string nameDir, string userName)
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(nameDir);
+                DirectorySecurity ds = dirInfo.GetAccessControl(AccessControlSections.Access);
+                //Только для папок
+                ds.AddAccessRule(new FileSystemAccessRule(userName,
+                    FileSystemRights.FullControl,
+                    InheritanceFlags.ContainerInherit,
+                    PropagationFlags.InheritOnly,
+                    AccessControlType.Denay));
+                //Только для файлов
+                ds.AddAccessRule(new FileSystemAccessRule(userName,
+                    FileSystemRights.FullControl,
+                    InheritanceFlags.ObjectInherit,
+                    PropagationFlags.InheritOnly,
+                    AccessControlType.Denay));
+                //Только для этой папки
+                ds.AddAccessRule(new FileSystemAccessRule(userName,
+                    FileSystemRights.FullControl,
+                    InheritanceFlags.None,
+                    PropagationFlags.InheritOnly,
+                    AccessControlType.Denay));
+                dirInfo.SetAccessControl(ds);
+                return true;
+            }
+            catch
+            { return false; }
+        }
+
+    }
+}
---
+++ SPO4lab/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+
+
+
+namespace SPO4lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\admin\Documents\учебка\3 курс\6 семестр\программное обеспечение\СПО4лаба";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+
+            Console.WriteLine($"Название каталога: {dirInfo.Name}");
+
+
+
+            
+        }
+    }
+}
